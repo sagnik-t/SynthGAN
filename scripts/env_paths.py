40659bfb48056aa9023c16c50e74425ba9a3c748
sagnik-t <code.sagnik@gmail.com>
2024-04-15 13:46:50 +0530

    Add path management script for generating .pth file and update dependency file

---
+++ scripts/env_paths.py
@@ -0,0 +1,19 @@
+import site
+from pathlib import Path
+
+root = Path.cwd().parent
+
+src_path = root / 'src'
+config_path = root / 'config'
+
+env_paths = [src_path, config_path]
+
+site_pckgs_dir = Path(site.getsitepackages()[0])
+
+pth_file = Path(site_pckgs_dir / 'env_paths.pth')
+
+Path.touch(pth_file)
+
+with open(pth_file, 'w') as file:
+    for path in env_paths:
+        file.write(path.as_posix() + '\n')
