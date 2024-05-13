4c7efafff6f695e5a5209654ccfa17dfcd39dff9
sagnik-t <code.sagnik@gmail.com>
2024-05-13 12:50:31 +0530

    Add docs to config.py and data_loader.py

--- config/config.py
+++ config/config.py
@@ -1,6 +1,18 @@
 from pathlib import Path
 
 class Config:
+    """
+    Configuration class for managing file paths within the project.
+
+    Attributes:
+        ROOT_PATH (Path): The root directory of the project, determined dynamically by the location of this file.
+        RAW_DATA_PATH (Path): The directory path for raw data files.
+        PROCESSED_DATA_PATH (Path): The directory path for processed data files.
+        RAW_DATA_FILE (Path): The file path for the raw dataset file.
+        TRAIN_DATA_FILE (Path): The file path for the training dataset.
+        VAL_DATA_FILE (Path): The file path for the validation dataset.
+        PIPELINE_PATH (Path): The directory path for storing pipeline-related files.
+    """
     
     ROOT_PATH = Path(__file__).parent.parent
     RAW_DATA_PATH = ROOT_PATH / 'data' / 'raw'
