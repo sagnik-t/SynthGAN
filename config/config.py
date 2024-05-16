d0cc90160b1a66ccfca0cfd6e43ffa74e7d2c8ca
sagnik-t <code.sagnik@gmail.com>
2024-05-16 21:58:40 +0530

    Refactor config file to use dataclasses and add wrapper class for .env variables

--- config/config.py
+++ config/config.py
@@ -1,23 +1,33 @@
+from dataclasses import dataclass, fields
+import os
 from pathlib import Path
 
 class Config:
-    """
-    Configuration class for managing file paths within the project.
-
-    Attributes:
-        ROOT_PATH (Path): The root directory of the project, determined dynamically by the location of this file.
-        RAW_DATA_PATH (Path): The directory path for raw data files.
-        PROCESSED_DATA_PATH (Path): The directory path for processed data files.
-        RAW_DATA_FILE (Path): The file path for the raw dataset file.
-        TRAIN_DATA_FILE (Path): The file path for the training dataset.
-        VAL_DATA_FILE (Path): The file path for the validation dataset.
-        PIPELINE_PATH (Path): The directory path for storing pipeline-related files.
-    """
     
-    ROOT_PATH = Path(__file__).parent.parent
-    RAW_DATA_PATH = ROOT_PATH / 'data' / 'raw'
-    PROCESSED_DATA_PATH = ROOT_PATH / 'data' / 'processed'
-    RAW_DATA_FILE = RAW_DATA_PATH / 'movielens100k.data'
-    TRAIN_DATA_FILE = PROCESSED_DATA_PATH / 'train-set.csv'
-    VAL_DATA_FILE = PROCESSED_DATA_PATH / 'val-set.csv'
-    PIPELINE_PATH = ROOT_PATH / 'pipe'
+    @dataclass
+    class Paths:
+        ROOT_PATH: Path = Path(__file__).parent.parent
+        RAW_DATA_PATH: Path = ROOT_PATH / 'data' / 'raw'
+        PROCESSED_DATA_PATH: Path = ROOT_PATH / 'data' / 'processed'
+        RAW_DATA_FILE: Path = RAW_DATA_PATH / 'movielens100k.data'
+        TRAIN_DATA_FILE: Path = PROCESSED_DATA_PATH / 'train-set.csv'
+        VAL_DATA_FILE: Path = PROCESSED_DATA_PATH / 'val-set.csv'
+        PIPELINE_PATH: Path = ROOT_PATH / 'pipe'
+    
+    @dataclass
+    class Vars:
+        TRAIN_NUM_USERS: int = int(os.getenv('TRAIN_NUM_USERS'))
+        TRAIN_NUM_ITEMS: int = int(os.getenv('TRAIN_NUM_ITEMS'))
+        VAL_NUM_USERS: int = int(os.getenv('VAL_NUM_USERS'))
+        VAL_NUM_ITERMS: int = int(os.getenv('VAL_NUM_ITEMS'))
+    
+    def __init__(self):
+        self.paths = self.Paths()
+        self.vars = self.Vars()
+
+if __name__=='__main__':
+    config = Config()
+    for path in fields(config.paths):
+        print(getattr(config.paths, path.name))
+    for var in fields(config.vars):
+        print(getattr(config.vars, var.name))
