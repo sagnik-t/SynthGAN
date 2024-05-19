0bff874ca049e9cc82b57fbd147bdc9790e7534a
sagnik-t <code.sagnik@gmail.com>
2024-05-19 20:15:02 +0530

    Update DeepMF model and update main noteboook

--- src/models/deepmf.py
+++ src/models/deepmf.py
@@ -5,59 +5,98 @@ from keras import Model, layers, optimizers, losses, metrics
 
 from config import Config
 from utils.data_loader import DataLoader
+from models.layers.split import Split
+from models.layers.normalization import MinMaxNormalization
 
 class DeepMF(Model):
     
-    def __init__(self, latent_dim=5, **kwargs):
-        super(DeepMF, self).__init__(**kwargs)
+    def __init__(
+        self,
+        latent_dim:int = 5, 
+        num_users:int = Config.Vars.NUM_USERS,
+        num_items:int = Config.Vars.NUM_ITEMS,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
         self.latent_dim = latent_dim
-        self.num_users = Config.Vars.NUM_USERS
-        self.num_items = Config.Vars.NUM_ITEMS
-        
+        self.num_users = num_users
+        self.num_items = num_items
+        self.split = Split(name='split')
+        self.rating = layers.Identity(name='rating')
         self.user_embedding = layers.Embedding(self.num_users + 1, self.latent_dim, name='user_embedding')
         self.user_flatten = layers.Flatten(name='user_flatten')
         self.item_embedding = layers.Embedding(self.num_items + 1, self.latent_dim, name='item_embedding')
         self.item_flatten = layers.Flatten(name='item_flatten')
         self.dot = layers.Dot(axes=1, name='dot')
+        self.concatenate1 = layers.Concatenate(axis=1, name='concatenate1')
+        self.norm = MinMaxNormalization(min_val=0, max_val=5, name='normalize')
+        self.reshape = layers.Reshape(target_shape=(-1,), name='reshape')
+        self.concatenate2 = layers.Concatenate(axis=1, name='concatenate2')
     
     def call(self, inputs):
-        user_input, item_input = inputs[0], inputs[1]
+        user_id, item_id, ratings = self.split(inputs)
+        user_emb = self.user_embedding(user_id)
+        user_emb = self.user_flatten(user_emb)
+        item_emb = self.item_embedding(item_id)
+        item_emb = self.item_flatten(item_emb)
+        rating_vec = self.dot([user_emb, item_emb])
+        embeddings = self.concatenate1([user_emb, item_emb])
+        ratings = self.rating(ratings)
+        norm_ratings = self.norm(ratings)
+        norm_ratings = self.reshape(norm_ratings)
+        embedded_data = self.concatenate2([embeddings, norm_ratings])
+        return rating_vec, embedded_data
+    
+    def predict(self, x):
+        _, embedded_data = self.call(x)
+        return embedded_data
+    
+    def train_step(self, data):
+        inputs, output = data
         
-        user_embedded = self.user_embedding(user_input)
-        user_flattened = self.user_flatten(user_embedded)
-        item_embedded = self.item_embedding(item_input)
-        item_flattened = self.item_flatten(item_embedded)
+        with tf.GradientTape() as tape:
+            rating_vec, _ = self.call(inputs)
+            loss = self.compute_loss(y=output, y_pred=rating_vec)
+            
+        grads = tape.gradient(loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
         
-        rating_vec = self.dot([user_flattened, item_flattened])
-        return rating_vec
+        return {
+            'mse_loss': loss,
+            **self.compute_metrics(x=inputs[0], y=output, y_pred=rating_vec)
+        }
     
     def summary(self):
-        x = layers.Input(shape=(2,))
-        return Model(inputs=x, outputs=self.call(x)).summary()
+        model = self.build_graph()
+        return model.summary()
     
     def build_graph(self):
-        x = layers.Input(shape=(2,))
+        x = (layers.Input(shape=(3,)))
         return Model(inputs=x, outputs=self.call(x))
 
 if __name__ == '__main__':
     
-    deepmf = DeepMF()
+    x_train, x_test, y_train, y_test = DataLoader().load_numpy()
     
+    deepmf = DeepMF()
     deepmf.compile(
         optimizer=optimizers.Adam(),
         loss=losses.MeanSquaredError(),
-        metrics=[keras.metrics.MeanSquaredError()]
+        metrics=[metrics.R2Score()]
     )
-    
-    x_train, x_test, y_train, y_test = DataLoader().load_numpy()
     
     deepmf.fit(
-    [x_train[:, 0], x_train[:, 1]],
-    y_train,
-    validation_data=([x_test[:, 0], x_test[:, 1]], y_test),
-    epochs=10,
-    batch_size=32,
-    validation_freq=3
+        [x_train[:, 0], x_train[:, 1], x_train[:, 2]],
+        y_train,
+        validation_data=([x_test[:, 0], x_test[:, 1], x_test[:, 2]], y_test),
+        epochs=10,
+        batch_size=32,
+        validation_freq=3
     )
-    
     deepmf.summary()
+    keras.utils.plot_model(
+        deepmf.build_graph(),
+        show_shapes=True,
+        dpi=70,
+        to_file='src/models/images/deepmf.png'
+    )
