aa95718bdf5a43a1fd4cc6d71d341016f7124d23
sagnik-t <code.sagnik@gmail.com>
2024-05-21 12:17:16 +0530

    Update GAN model

--- src/models/gan.py
+++ src/models/gan.py
@@ -2,28 +2,28 @@ import tensorflow as tf
 import keras
 from keras import Model, layers, losses
 
-
+@keras.utils.register_keras_serializable(package='Custom', name='GAN')
 class GAN(Model):
     
     def __init__(
         self,
         generator: Model,
-        discrimator: Model,
-        latent_dim: int = 100,
+        discriminator: Model,
+        noise_dim: int = 100,
         **kwargs
     ):
         super(GAN, self).__init__(**kwargs)
+        self.noise_dim = noise_dim
         self.generator = generator
-        self.discrimator = discrimator
-        self.latent_dim = latent_dim
+        self.discriminator = discriminator
     
     def call(self, inputs, training=False):
         noise_vec, input_tensor = inputs
         
         gen_tensor = self.generator(noise_vec, training=True)
         
-        real_output = self.discrimator(input_tensor, training=True)
-        gen_output = self.discrimator(gen_tensor, training=True)
+        real_output = self.discriminator(input_tensor, training=True)
+        gen_output = self.discriminator(gen_tensor, training=True)
         
         return gen_tensor, real_output, gen_output
     
@@ -50,17 +50,17 @@ class GAN(Model):
     
     def train_step(self, inputs):
         
-        noise_vec = tf.random.normal((tf.shape(inputs)[0], self.latent_dim))
+        noise_vec = tf.random.normal((tf.shape(inputs)[0], self.noise_dim))
         
         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
             _, real_output, gen_output = self.call(inputs=(noise_vec, inputs), training=True)
-            loss = self.compute_loss(y=[inputs, real_output, gen_output])
+            loss = self.compute_loss(y_pred=[inputs, real_output, gen_output])
         
         gen_grads = gen_tape.gradient(loss['gen_loss'], self.generator.trainable_weights)
-        disc_grads = disc_tape.gradient(loss['disc_loss'], self.discrimator.trainable_weights)
+        disc_grads = disc_tape.gradient(loss['disc_loss'], self.discriminator.trainable_weights)
         
         self.gen_optimizer.apply_gradients(zip(gen_grads, self.generator.trainable_weights))
-        self.disc_optimizer.apply_gradients(zip(disc_grads, self.discrimator.trainable_weights))
+        self.disc_optimizer.apply_gradients(zip(disc_grads, self.discriminator.trainable_weights))
         
         return {
             'gen_loss': loss['gen_loss'],
@@ -70,17 +70,17 @@ class GAN(Model):
     def predict(self, x):
         return self.generator(x)
     
-    def summary(self):
+    def summary(self, **kwargs):
         model = self.build_graph()
-        return model.summary()
+        return model.summary(**kwargs)
     
     def build_graph(self):
-        if isinstance(self.discrimator, keras.src.models.Functional):
-            input_shape = self.discrimator.layers[1].input.shape[1:]
+        if isinstance(self.discriminator, keras.src.models.Functional):
+            input_shape = self.discriminator.layers[1].input.shape[1:]
         else:
-            input_shape = self.discrimator.layers[0].input.shape[1:]
+            input_shape = self.discriminator.layers[0].input.shape[0:]
         
         input_tensor = layers.Input(shape=input_shape)
-        noise_vec = layers.Input(shape=(self.latent_dim,))
+        noise_vec = layers.Input(shape=(self.noise_dim,))
         
-        return Model(inputs=[noise_vec, input_tensor], outputs=self.call([noise_vec, input_tensor]))
+        return Model(inputs=[noise_vec, input_tensor], outputs=self.call((noise_vec, input_tensor)))
